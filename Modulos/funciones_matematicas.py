0760dea049c2f29fde22abd25a0db79993a48273
Nattitor <ryufg.100@gmail.com>
2024-06-24 17:50:57 -0400

    Print y operación

---
+++ Modulos/funciones_matematicas.py
@@ -0,0 +1,10 @@
+"""Este módulo permite hacer funciones matemáticas básicas"""
+
+def sumar(op1,op2):
+    print("El resultado de la suma es: ", op1+op2)
+    
+def restar(op1,op2):
+    print("El resultado de la resta es: ", op1-op2)
+    
+def multiplicar(op1,op2):
+    print("El resultado de la multiplicación es: ", op1*op2)
